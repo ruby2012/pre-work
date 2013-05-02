d8adb7b9de43960495f12e16b414003b8cc37241
tnewbie <tnewbie@server.fake>
2013-05-02 12:35:28 -0400

    initial commit

---
+++ ch5exercise.rb
@@ -0,0 +1,7 @@
+puts 'What is your First name?'
+name1 = gets.chomp
+puts 'What is your middle name?'
+name2 = gets.chomp
+puts 'What is your last name'
+name3 = gets.chomp
+puts 'Hello, ' + name1 +' ' + name2 +' ' + name3 +'.'
---
+++ ch5exercise2.rb
@@ -0,0 +1,5 @@
+puts 'What is your favorite number?'
+number1 = gets.chomp
+number2 = number1.to_i + 1
+puts 'Guess what ' + number2.to_s + ' is bigger and better than ' +number1+'!'
+
