3e0fd77c8934b4cdc2d09f8c53e8e9a3507e63ac
tnewbie <tnewbie@server.fake>
2013-05-03 16:38:03 -0400

    updated assignments

---
+++ chp7exercise.rb
@@ -0,0 +1,12 @@
+count = 100
+97.times do |variable|
+	count -= 1
+	puts count.to_s + ' Bottles of beer on the wall ' + count.to_s + 
+	' bottles of beer, take one down pass it around,'
+end
+puts "2 bottles of beer on the wall, 2 bottle of beer."
+puts "Take one down and pass it around, 1 bottles of beer on wall."
+puts "1 bottle of beer on the wall, 1 bottle of beer."
+puts "Take one down and pass it around, no more bottles of beer on the wall"
+puts "No more bottles of beer on the wall, no more bottles of beer"
+puts "Go to the store and buy some, 99 bottles of beer on the wall"
